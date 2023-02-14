83ddb5bce6794eb22fcca38acb6f7924c1049ab9
VietIS Bach <bach.ngongoc@vietis.com.vn>
2023-02-14 11:32:30 +0700

    ADD IMG Logo

--- wp-content/plugins/wedding/build/blocks.asset.php
+++ wp-content/plugins/wedding/build/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '520682360299c0b7a400');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '291299ed2d11d1641744');
